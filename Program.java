df2c6d7d89792974e4c28734ace4dc446d8842e6
Butvila_Antanas <antanas.werwolf@gmail.com>
2024-03-28 11:46:39 +0300

    Initial commit

---
+++ Program.java
@@ -0,0 +1,7 @@
+public class Program{
+    public static void main(String[] args) {
+        System.out.println("Введите заказ: ");
+        Order order = new Create().inputFromConsole();
+        new SaveToJson(order).save();
+    }
+}
